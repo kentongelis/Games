7fac2f680ba89e69a6ba72b7e921365a25595491
kentongelis <kenton.gelis@students.dominican.edu>
2024-10-16 15:55:24 -0700

    updating

--- games_app/config.py
+++ games_app/config.py
@@ -3,10 +3,11 @@ import os
 
 load_dotenv()
 
+
 class Config(object):
     """Set environment variables."""
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_PATH = os.getenv('UPLOAD_PATH')
+    UPLOAD_PATH = os.getenv("UPLOAD_PATH")
